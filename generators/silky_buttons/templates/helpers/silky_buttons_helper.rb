fa5e132b52b4095c7c8caa243ddfae2c36e09c35
Guillermo Asernitzky <guillea@gmail.com>
2010-09-10 16:04:13 -0700

    Method new_button accepts class as parameter. Method submit_button accepts resource as parameter submit_button uses localized text and detects action to show based on object state, like Rails 3.0 (taken from actionpack-3.0.0)

--- generators/silky_buttons/templates/helpers/silky_buttons_helper.rb
+++ generators/silky_buttons/templates/helpers/silky_buttons_helper.rb
@@ -1,11 +1,11 @@
 module SilkyButtonsHelper
 
-  def silk_image( img, options={} )
+  def silk_image(img, options={})
     image_tag "/images/silk/icons/#{img}", options
   end
 
-  def submit_button(text, options={})
-    options[:text]      = text
+  def submit_button(resource, options={})
+    options[:text]      = submit_text(resource)
     options[:type]      = "submit"
     options[:icon]      ||= "tick.png"
     unless options[:icon].blank?
@@ -62,15 +62,34 @@ module SilkyButtonsHelper
     resource_button(resource, options)
   end
 
-  def resource_button(resource, options={})
-    options[:text]        ||= resource.class.model_name.human
-    options[:path]        ||= polymorphic_path(resource)
-    unless options[:icon].blank?
-      options[:icon] = silk_image(options[:icon])
-      options[:text] = "#{options[:icon]} #{options[:text]}"
+  private
+    def resource_button(resource, options={})
+      options[:text]        ||= resource.class.model_name.human
+      options[:path]        ||= polymorphic_path(resource)
+      unless options[:icon].blank?
+        options[:icon] = silk_image(options[:icon])
+        options[:text] = "#{options[:icon]} #{options[:text]}"
+      end
+      options.merge!({ :class => "button #{options[:class]}" })
+      link_to options[:text].html_safe, options[:path], options.delete_if { |k, v| [:icon, :text, :path].include? k }
     end
-    options.merge!({ :class => "button #{options[:class]}" })
-    link_to options[:text].html_safe, options[:path], options.delete_if { |k, v| [:icon, :text, :path].include? k }
-  end
-  
+
+    # adapted from module 'form_helper.rb' in 'actionpack-3.0.0'
+    def submit_text(resource)
+      object = resource.respond_to?(:to_model) ? resource.to_model : resource
+      key    = object ? (object.persisted? ? :update : :create) : :submit
+
+      model = if object.class.respond_to?(:model_name)
+        object.class.model_name.human
+      else
+        resource.to_s.humanize
+      end
+
+      defaults = []
+      defaults << :"helpers.submit.#{object.to_s}.#{key}"
+      defaults << :"helpers.submit.#{key}"
+      defaults << "#{key.to_s.humanize} #{model}"
+
+      I18n.t(defaults.shift, :model => model, :default => defaults)
+    end
 end
